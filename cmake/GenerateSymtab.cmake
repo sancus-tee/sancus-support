927f4055f6c52d7e7f9a4eacfbbd1d72f2b2c963
Job Noorman <job.noorman@cs.kuleuven.be>
2014-04-09 17:42:22 +0200

    Add possibility to add symbols from standard libraries to the symtab.

--- cmake/GenerateSymtab.cmake
+++ cmake/GenerateSymtab.cmake
@@ -1,7 +1,13 @@
-function(generate_symtab output input)
+include(CMakeParseArguments)
+
+function(generate_symtab)
     if (NOT EXISTS ${SANCUS_SUPPORT_GENSYMTAB})
         message(FATAL_ERROR "Cannot find suitable symtab generator script")
     endif ()
+
+    cmake_parse_arguments(ARG "" "" "STD_LIBS" ${ARGN})
+    list(GET ARG_UNPARSED_ARGUMENTS 0 output)
+    list(GET ARG_UNPARSED_ARGUMENTS 1 input)
 
     # the list of input files is the given target library and all its
     # dependencies
@@ -16,7 +22,13 @@ function(generate_symtab output input)
         set(inputs ${inputs} $<TARGET_FILE:${lib}>)
     endforeach ()
 
+    set(options -o ${output} ${inputs})
+
+    foreach (lib ${ARG_STD_LIBS})
+        set(options ${options} --add-std-lib ${lib})
+    endforeach ()
+
     add_custom_command(OUTPUT  ${output}
-                       COMMAND ${SANCUS_SUPPORT_GENSYMTAB} -o ${output} ${inputs}
+                       COMMAND ${SANCUS_SUPPORT_GENSYMTAB} ${options}
                        DEPENDS ${input} ${libs})
 endfunction()
--- tools/generate-symtab.py
+++ tools/generate-symtab.py
@@ -7,6 +7,21 @@ import os
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 
+
+def _find_std_lib(name):
+    libc = subprocess.check_output(['msp430-gcc', '-print-file-name=libc.a'],
+                                   universal_newlines=True)
+    libdir = os.path.dirname(libc)
+    files = (fmt.format(name) for fmt in ('{}', 'lib{}.a', '{}.a'))
+
+    for file in files:
+        path = '{}/{}'.format(libdir, file)
+        if os.path.isfile(path):
+            return path
+
+    raise argparse.ArgumentTypeError('Unable to find library "{}"'.format(name))
+
+
 def _extract_ar(ar_file):
     tmp_dir = tempfile.mkdtemp()
     subprocess.check_call(['ar', 'x', ar_file], cwd=tmp_dir)
@@ -29,8 +44,23 @@ def _generate_symbols(inputs):
                     if isinstance(section, SymbolTableSection):
                         for symbol in section.iter_symbols():
                             if _should_add_symbol(symbol):
-                                yield symbol.name.decode('ascii')
+                                yield symbol
 
+
+def _emit_symbols(inputs, ignores=[]):
+    decl_lines = []
+    sym_lines = []
+
+    for symbol in _generate_symbols(inputs):
+        name = symbol.name.decode('ascii')
+        if not name in ignores:
+            type = symbol['st_info']['type']
+            decl_lines.append('extern char {};'.format(name))
+            sym_lines.append('    {{"{0}", &{0}, 0}}'.format(name))
+            if args.verbose:
+                print('Added symbol', name)
+
+    return (decl_lines, sym_lines)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output',
@@ -38,28 +68,37 @@ parser.add_argument('-o', '--output',
 parser.add_argument('inputs',
                     nargs='+',
                     help='Input files')
+parser.add_argument('--add-std-lib',
+                    action='append',
+                    default=[],
+                    type=_find_std_lib,
+                    dest='additional_libs',
+                    help='Add a standard library to the symbol table')
 parser.add_argument('-v', '--verbose',
+                    action='store_true',
                     help='Be verbose')
 args = parser.parse_args()
 
+includes = ('sancus_support/private/symbol.h', 'errno.h', 'math.h', 'stdint.h',
+            'stdio.h', 'stdlib.h', 'string.h', 'ctype.h', 'byteswap.h',
+            'setjmp.h')
+
+input_decls, input_syms = _emit_symbols(args.inputs, ignores=['putchar'])
+_, libs_syms = _emit_symbols(args.additional_libs,
+                             ignores=['ffs', 'rindex', 'index'])
+
 lines = []
-lines.append('#include <stdlib.h>')
-lines.append('#include <sancus_support/private/symbol.h>')
-decl_lines = []
-sym_lines = []
 
-for symbol in _generate_symbols(args.inputs):
-    decl_lines.append('extern char {};'.format(symbol))
-    sym_lines.append('    {{"{0}", &{0}, 0}}'.format(symbol))
-    if args.verbose:
-        print('Added symbol', symbol)
+for include in includes:
+    lines.append('#include <{}>'.format(include))
 
-lines.append('\n'.join(decl_lines))
+lines.append('\n'.join(input_decls))
 lines.append('const Symbol static_symbols[] = {')
-lines.append(',\n'.join(sym_lines))
+lines.append(',\n'.join(input_syms + libs_syms))
 lines.append('};')
-lines.append('const size_t num_static_symbols = ' +
+lines.append('const unsigned num_static_symbols = ' +
              'sizeof(static_symbols) / sizeof(Symbol);')
+
 data = '\n'.join(lines)
 
 with open(args.output, 'wb') as f:
