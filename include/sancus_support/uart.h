61962d24277fe6d3f259ca4a1ffd8273b19e21f7
Job Noorman <job.noorman@cs.kuleuven.be>
2014-04-24 16:29:42 +0200

    Receive UART bytes in a circular buffer.

--- include/sancus_support/uart.h
+++ include/sancus_support/uart.h
@@ -4,11 +4,13 @@
 #include <stddef.h>
 
 void uart_init(void);
-int uart_available(void);
+size_t uart_available(void);
 void uart_write_byte(unsigned char b);
-void uart2_write_byte(unsigned char b);
 void uart_write(const unsigned char* buf, size_t size);
 unsigned char uart_read_byte(void);
 void uart_read(unsigned char* buf, size_t size);
+void uart_flush(void);
+
+void uart2_write_byte(unsigned char b);
 
 #endif
--- src/dev/uart.c
+++ src/dev/uart.c
@@ -3,20 +3,39 @@
 #include "uart.h"
 #include "uart_hardware.h"
 
-static volatile unsigned char rxdata;
-static volatile int data_ready = 0;
+#include <stdint.h>
+
+#define RX_BUFFER_SIZE 128
+
+// one element is wasted in the buffer to be able to make a distinction between
+// a full and an empty buffer.
+static volatile uint8_t rx_buffer[RX_BUFFER_SIZE];
+
+// points to the index where the next element should be written
+static volatile size_t rx_head;
+
+// points to the index where the next element should be read iff not equal to
+// rx_head, in which case the buffer is empty
+static volatile size_t rx_tail;
 
 void uart_init(void )
 {
+    rx_head = rx_tail = 0;
     UART_BAUD = BAUD;
     UART_CTL = UART_EN | UART_IEN_RX;
     UART2_BAUD = BAUD;
     UART2_CTL = UART_EN;
 }
 
-int uart_available(void)
+size_t uart_available(void)
 {
-    return data_ready;
+    size_t head = rx_head;
+    size_t tail = rx_tail;
+
+    if (head >= tail)
+        return head - tail;
+    else
+        return RX_BUFFER_SIZE + head - tail;
 }
 
 void uart_write_byte(unsigned char b)
@@ -28,15 +47,6 @@ void uart_write_byte(unsigned char b)
     UART_TXD = b;
 }
 
-void uart2_write_byte(unsigned char b)
-{
-    // wait while TX buffer is full
-    while (UART2_STAT & UART_TX_FULL) {}
-
-    // write byte to TX buffer
-    UART2_TXD = b;
-}
-
 void uart_write(const unsigned char* buf, size_t size)
 {
     while (size--)
@@ -45,9 +55,13 @@ void uart_write(const unsigned char* buf, size_t size)
 
 unsigned char uart_read_byte(void)
 {
-    while (!data_ready) {}
-    data_ready = 0;
-    return rxdata;
+    size_t i = rx_tail;
+
+    while (i == rx_head) {}
+
+    uint8_t ret = rx_buffer[i];
+    rx_tail = (i + 1) % RX_BUFFER_SIZE;
+    return ret;
 }
 
 void uart_read(unsigned char* buf, size_t size)
@@ -56,13 +70,34 @@ void uart_read(unsigned char* buf, size_t size)
         *buf++ = uart_read_byte();
 }
 
+void uart_flush(void)
+{
+    while (UART_STAT & UART_TX_BUSY) {}
+}
+
 static void __attribute__((interrupt(UART_RX_VECTOR))) uart_receive(void)
 {
     // Read the received data
-    rxdata = UART_RXD;
-    data_ready = 1;
+    uint8_t byte = UART_RXD;
+
+    size_t i = rx_head;
+    size_t next_head = (i + 1) % RX_BUFFER_SIZE;
+
+    if (next_head != rx_tail) // drop byte if buffer is full
+    {
+        rx_buffer[i] = byte;
+        rx_head = next_head;
+    }
 
     // Clear the receive pending flag
     UART_STAT = UART_RX_PND;
 }
 
+void uart2_write_byte(unsigned char b)
+{
+    // wait while TX buffer is full
+    while (UART2_STAT & UART_TX_FULL) {}
+
+    // write byte to TX buffer
+    UART2_TXD = b;
+}
