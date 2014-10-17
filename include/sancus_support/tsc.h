587a0143c1ce74bc37deace1d4b6dfce0359fc1c
Job Noorman <job.noorman@cs.kuleuven.be>
2014-10-17 16:18:26 +0200

    Make tsc_read inline.

--- include/sancus_support/tsc.h
+++ include/sancus_support/tsc.h
@@ -3,8 +3,17 @@
 
 #include <stdint.h>
 
+#define TSC_VAL (*(volatile tsc_t*)  0x0190)
+#define TSC_CTL (*(volatile uint8_t*)0x0190)
+
 typedef uint64_t tsc_t;
 
-tsc_t tsc_read(void);
+inline tsc_t tsc_read(void)
+{
+    // any write to TSC_CTL takes as snapshot of the current value which can be
+    // read from TSC_VAL later
+    TSC_CTL = 1;
+    return TSC_VAL;
+}
 
 #endif
