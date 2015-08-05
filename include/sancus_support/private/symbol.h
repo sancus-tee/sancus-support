7079ecb2f3a97aaa0da796b85548d490794ee0fc
Job Noorman <job.noorman@cs.kuleuven.be>
2015-08-05 10:59:55 +0200

    Add helper macros to define a static symbol table.

--- include/sancus_support/private/symbol.h
+++ include/sancus_support/private/symbol.h
@@ -7,4 +7,10 @@ typedef struct
     void*       value;
 } Symbol;
 
+#define STATIC_SYMBOLS_START const Symbol static_symbols[] = {
+#define SYMBOL(name) {#name, &name}
+#define STATIC_SYMBOLS_END \
+    }; \
+    const unsigned num_static_symbols = sizeof(static_symbols) / sizeof(Symbol);
+
 #endif
