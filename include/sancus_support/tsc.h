9c3a2b4fd260ade751d3faa745c9978a63521200
Job Noorman <job@noorman.info>
2016-02-13 16:18:22 +0100

    Add some TSC helper macros for consistent timings.
    These macros are implemented in assembly to rule out timing fluctuations caused
    by how the compiler compiles the C code of tsc_read().

--- include/sancus_support/tsc.h
+++ include/sancus_support/tsc.h
@@ -16,4 +16,30 @@ static inline __attribute__((always_inline)) tsc_t tsc_read(void)
     return TSC_VAL;
 }
 
+#define DECLARE_TSC_TIMER(name)                                     \
+    struct {tsc_t start, end;} name;                                \
+    static void __attribute__((noinline)) name##_print_interval()   \
+    {                                                               \
+        printf(#name ": %llu\n", name.end - name.start - 28);       \
+    }
+
+#define TSC_TIMER_START(name)                               \
+    do {                                                    \
+        asm volatile("mov.b #1, &0x0190\n\t"                \
+                     "mov.w &0x0190, &" #name "+0\n\t"      \
+                     "mov.w &0x0192, &" #name "+2\n\t"      \
+                     "mov.w &0x0194, &" #name "+4\n\t"      \
+                     "mov.w &0x0196, &" #name "+6\n\t");    \
+    } while (0)
+
+#define TSC_TIMER_END(name)                                 \
+    do {                                                    \
+        asm volatile("mov.b #1, &0x0190\n\t"                \
+                     "mov.w &0x0190, &" #name "+8\n\t"      \
+                     "mov.w &0x0192, &" #name "+10\n\t"     \
+                     "mov.w &0x0194, &" #name "+12\n\t"     \
+                     "mov.w &0x0196, &" #name "+14\n\t");   \
+        name##_print_interval();                            \
+    } while (0)
+
 #endif
