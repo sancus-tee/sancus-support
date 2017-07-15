7110ad5722b9a8db43fb836e978f96bd3cec0cd5
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2017-07-15 02:49:15 +0200

    Parameterized TSC peripheral MMIO addresses.

--- include/sancus_support/tsc.h
+++ include/sancus_support/tsc.h
@@ -3,15 +3,18 @@
 
 #include <stdint.h>
 
-#define TSC_VAL (*(volatile tsc_t*)  0x0190)
-#define TSC_CTL (*(volatile uint8_t*)0x0190)
+#define TSC_BASE    0x190
+#define TSC_SIZE    8
+
+// any write to TSC_CTL takes as snapshot of the current value which can be
+// read from TSC_VAL later
+#define TSC_VAL (*(volatile tsc_t*)   TSC_BASE)
+#define TSC_CTL (*(volatile uint8_t*) TSC_BASE)
 
 typedef uint64_t tsc_t;
 
 static inline __attribute__((always_inline)) tsc_t tsc_read(void)
 {
-    // any write to TSC_CTL takes as snapshot of the current value which can be
-    // read from TSC_VAL later
     TSC_CTL = 1;
     return TSC_VAL;
 }
@@ -25,20 +28,22 @@ static inline __attribute__((always_inline)) tsc_t tsc_read(void)
 
 #define TSC_TIMER_START(name)                               \
     do {                                                    \
-        asm volatile("mov.b #1, &0x0190\n\t"                \
-                     "mov.w &0x0190, &" #name "+0\n\t"      \
-                     "mov.w &0x0192, &" #name "+2\n\t"      \
-                     "mov.w &0x0194, &" #name "+4\n\t"      \
-                     "mov.w &0x0196, &" #name "+6\n\t");    \
+        asm volatile("mov.b #1, %0\n\t"                     \
+                     "mov.w %1+0, &" #name "+0\n\t"         \
+                     "mov.w %1+2, &" #name "+2\n\t"         \
+                     "mov.w %1+4, &" #name "+4\n\t"         \
+                     "mov.w %1+6, &" #name "+6\n\t"         \
+                     :: "m"(TSC_CTL), "m"(TSC_VAL):);       \
     } while (0)
 
 #define TSC_TIMER_END(name)                                 \
     do {                                                    \
-        asm volatile("mov.b #1, &0x0190\n\t"                \
-                     "mov.w &0x0190, &" #name "+8\n\t"      \
-                     "mov.w &0x0192, &" #name "+10\n\t"     \
-                     "mov.w &0x0194, &" #name "+12\n\t"     \
-                     "mov.w &0x0196, &" #name "+14\n\t");   \
+        asm volatile("mov.b #1, %0\n\t"                     \
+                     "mov.w %1+0, &" #name "+8\n\t"         \
+                     "mov.w %1+2, &" #name "+10\n\t"        \
+                     "mov.w %1+4, &" #name "+12\n\t"        \
+                     "mov.w %1+6, &" #name "+14\n\t"        \
+                     :: "m"(TSC_CTL), "m"(TSC_VAL):);       \
         name##_print_interval();                            \
     } while (0)
 
