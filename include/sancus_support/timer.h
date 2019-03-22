60ba0fe9ad231a534c70cb685398e653fe3c37aa
Sven Cuyt <svecify@gmail.com>
2019-03-22 17:49:02 +0100

    Fixed macro for Timer ISR

--- include/sancus_support/timer.h
+++ include/sancus_support/timer.h
@@ -41,8 +41,34 @@ void timerA_isr_entry(void)                                         \
             "jne 1f\n\t"                                            \
             "mov &__isr_sp, r1\n\t"                                 \
             "push r15\n\t"                                          \
-            "push #0x0\n\t"                                         \
-            "1: call #" #fct "\n\t"                                 \
+            "push r2\n\t"                                           \
+            "1: push r15\n\t"                                       \
+            "push r14\n\t"                                          \
+            "push r13\n\t"                                          \
+            "push r12\n\t"                                          \
+            "push r11\n\t"                                          \
+            "push r10\n\t"                                          \
+            "push r9\n\t"                                           \
+            "push r8\n\t"                                           \
+            "push r7\n\t"                                           \
+            "push r6\n\t"                                           \
+            "push r5\n\t"                                           \
+            "push r4\n\t"                                           \
+            "push r3\n\t"                                           \
+            "call #" #fct "\n\t"                                    \
+            "pop r3\n\t"                                            \
+            "pop r4\n\t"                                            \
+            "pop r5\n\t"                                            \
+            "pop r6\n\t"                                            \
+            "pop r7\n\t"                                            \
+            "pop r8\n\t"                                            \
+            "pop r9\n\t"                                            \
+            "pop r10\n\t"                                           \
+            "pop r11\n\t"                                           \
+            "pop r12\n\t"                                           \
+            "pop r13\n\t"                                           \
+            "pop r14\n\t"                                           \
+            "pop r15\n\t"                                           \
             "reti\n\t"                                              \
             :::);                                                   \
 }
