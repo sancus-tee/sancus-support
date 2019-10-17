e954682e8392993d7a18e3debe2c78f2e05897e9
marton bognar <martonbognar@sch.bme.hu>
2019-10-17 23:17:34 +0200

    Remove unused, duplicated macro

--- include/sancus_support/timer.h
+++ include/sancus_support/timer.h
@@ -83,45 +83,4 @@ void timerA_isr_entry(void)                                         \
             :::);                                                   \
 }
 
-#define TIMER_ISR_ENTRY2(fct)                                       \
-__attribute__((naked)) __attribute__((interrupt(TIMER_IRQ_VECTOR2)))\
-void timerA_isr_entry2(void)                                        \
-{                                                                   \
-    __asm__ __volatile__(                                           \
-            "cmp #0x0, r1\n\t"                                      \
-            "jne 1f\n\t"                                            \
-            "mov &__isr_sp, r1\n\t"                                 \
-            "push r15\n\t"                                          \
-            "push r2\n\t"                                           \
-            "1: push r15\n\t"                                       \
-            "push r14\n\t"                                          \
-            "push r13\n\t"                                          \
-            "push r12\n\t"                                          \
-            "push r11\n\t"                                          \
-            "push r10\n\t"                                          \
-            "push r9\n\t"                                           \
-            "push r8\n\t"                                           \
-            "push r7\n\t"                                           \
-            "push r6\n\t"                                           \
-            "push r5\n\t"                                           \
-            "push r4\n\t"                                           \
-            "push r3\n\t"                                           \
-            "call #" #fct "\n\t"                                    \
-            "pop r3\n\t"                                            \
-            "pop r4\n\t"                                            \
-            "pop r5\n\t"                                            \
-            "pop r6\n\t"                                            \
-            "pop r7\n\t"                                            \
-            "pop r8\n\t"                                            \
-            "pop r9\n\t"                                            \
-            "pop r10\n\t"                                           \
-            "pop r11\n\t"                                           \
-            "pop r12\n\t"                                           \
-            "pop r13\n\t"                                           \
-            "pop r14\n\t"                                           \
-            "pop r15\n\t"                                           \
-            "reti\n\t"                                              \
-            :::);                                                   \
-}
-
 #endif
